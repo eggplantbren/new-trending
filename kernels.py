fde453f7da16c1dedf491eb0017d228e829d147e
Brendon J. Brewer <brendon.brewer@gmail.com>
2020-08-12 16:00:41 +1200

    Something like this could be nice

--- kernels.py
+++ kernels.py
@@ -3,8 +3,10 @@ Experimenting with kernels that ramp up starting immediately
 but have different shapes when the LBC values are different.
 """
 
+import math
 import matplotlib.pyplot as plt
-import numpy as np
+
+DECAY_TIME = 500
 
 class Kernel:
 
@@ -12,11 +14,38 @@ class Kernel:
         self.t0, self.lbc = t0, lbc
 
     def evaluate(self, ts):
-        ys = np.zeros(ts.shape)
+
+        # Peak time and amplitude
+        delay = min([1E3, self.lbc**0.4])
+        t_peak = self.t0 + delay
+        t_final = t_peak + DECAY_TIME
+        y_peak = self.lbc**0.25
+        if self.lbc <= 100.0:
+            power = 1.0
+        else:
+            power = 1.0 + math.log10(self.lbc/100.0)
+
+        # Create the kernel
+        frac = []
+        for t in ts:
+            if t < self.t0:
+                frac.append(0.0)
+            elif t < t_peak:
+                frac.append(1.0 - (t_peak - t)/delay)
+            elif t >= t_peak and t < t_final:
+                frac.append(1.0 - (t - t_peak)/DECAY_TIME)
+            else:
+                frac.append(0.0)
+
+        # Multiply in amplitude, also do the power
+        ys = []
+        for f in frac:
+            ys.append(f**power * y_peak)
+
         return ys
 
 
-ts = np.arange(1000)
+ts = range(1000)
 plt.plot(ts, Kernel(0.0, 100.0).evaluate(ts))
 plt.plot(ts, Kernel(0.0, 10000.0).evaluate(ts))
 plt.plot(ts, Kernel(0.0, 1000000.0).evaluate(ts))
