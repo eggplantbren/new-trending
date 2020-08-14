84a9dbbb84038575dc2d68f3de3de1756560e34d
Brendon J. Brewer <brendon.brewer@gmail.com>
2020-08-14 13:42:14 +1200

    rubbish

--- doc/integrals.py
+++ doc/integrals.py
@@ -1,23 +1,63 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+from sympy import *
 
-def evaluate(ts, kernel):
-    t0, A, delta, ell, alpha = kernel
-    t1 = t0 + delta
-    t2 = t1 + ell
-    ys = np.zeros(len(ts))
-    rise = (ts >= t0) & (ts < t1)
-    fall = (ts >= t1) & (ts < t2)
-    ys[rise] = A*((ts[rise] - t0)/delta)**alpha
-    ys[fall] = A*((t2 - ts[fall])/ell)**alpha
-    return ys
+# Kernel parameters
+t0, A, delta, ell, alpha = symbols("t0 A delta ell alpha")
 
-kernel = [40.0, 3.2, 30.0, 500.0, 2.0]
-t0, A, delta, ell, alpha = kernel
+# Derived kernel parameters
+t1 = t0 + delta
+t2 = t1 + ell
 
-ts = np.linspace(0, 1000.0, 100001)
-ys = evaluate(ts, kernel)
+# Kernel function
+t = symbols("t")
+K = Piecewise((0, t < t0),
+              (A*((t - t0)/delta)**alpha, t < t1),
+              (A*((t2 - t)/ell)**alpha, t < t2),
+              (0, True))
 
-plt.plot(ts, ys)
-plt.show()
+
+# Assumptions
+from sympy.assumptions.assume import global_assumptions
+global_assumptions.clear()
+
+# Entropy
+facts = Q.positive(A), Q.positive(delta), Q.positive(ell), Q.positive(alpha)
+with assuming(*facts):
+    H = integrate(K, (t, t0, t2))
+    print(H)
+
+#ts = np.linspace(0.0, 1000.0, 10001)
+#ys = np.empty(len(ts))
+#for i in range(len(ts)):
+#    e = K.evalf(subs=dict(t=ts[i], t0=20.0, A=5.0, delta=100.0, ell=500.0,
+#                                   alpha=2.0))
+#    ys[i] = e
+#    print(ts[i], ys[i])
+#plt.plot(ts, ys)
+#plt.show()
+
+#import numpy as np
+#import matplotlib.pyplot as plt
+
+#def evaluate(ts, kernel):
+#    t0, A, delta, ell, alpha = kernel
+#    t1 = t0 + delta
+#    t2 = t1 + ell
+#    ys = np.zeros(len(ts))
+#    rise = (ts >= t0) & (ts < t1)
+#    fall = (ts >= t1) & (ts < t2)
+#    ys[rise] = A*((ts[rise] - t0)/delta)**alpha
+#    ys[fall] = A*((t2 - ts[fall])/ell)**alpha
+#    return ys
+
+#kernel = [40.0, 3.2, 30.0, 500.0, 2.0]
+#t0, A, delta, ell, alpha = kernel
+
+#ts = np.linspace(0, 1000.0, 100001)
+#ys = evaluate(ts, kernel)
+
+#plt.plot(ts, ys)
+#plt.show()
+
 
