6d97bce7e8853130ff16db1bca68770219eb1db9
Brendon J. Brewer <brendon.brewer@gmail.com>
2020-08-15 13:28:43 +1200

    Biexponential

--- doc/integrals.py
+++ doc/integrals.py
@@ -43,12 +43,11 @@ import matplotlib.pyplot as plt
 def evaluate(ts, kernel):
     t0, A, delta, ell, alpha = kernel
     t1 = t0 + delta
-    t2 = t1 + ell
     ys = np.zeros(len(ts))
     rise = (ts >= t0) & (ts < t1)
-    fall = (ts >= t1) & (ts < t2)
-    ys[rise] = A*((ts[rise] - t0)/delta)**(alpha - 1)
-    ys[fall] = A*((t2 - ts[fall])/ell)**(alpha - 1)
+    fall = (ts >= t1)
+    ys[rise] = A*np.exp(2.0*(ts[rise] - t1)/delta)
+    ys[fall] = A*np.exp((t1 - ts[fall])/ell)
     return ys
 
 kernel = [40.0, 3.2, 30.0, 500.0, 2.5]
@@ -56,7 +55,7 @@ t0, A, delta, ell, alpha = kernel
 
 ts = np.linspace(0, 1000.0, 100001)
 ys = evaluate(ts, kernel)
-print(np.trapz(ys, x=ts), A*(delta + ell)/alpha)
+#print(np.trapz(ys, x=ts), A*(delta + ell)/alpha)
 
 plt.plot(ts, ys)
 plt.show()
