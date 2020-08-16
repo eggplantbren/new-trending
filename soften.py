839b5ef9dce04be3b799569ac4666f4f9704dea6
Brendon J. Brewer <brendon.brewer@gmail.com>
2020-08-16 13:18:27 +1200

    Softening function in its own file

---
+++ soften.py
@@ -0,0 +1,29 @@
+import math
+
+def soften(lbc):
+    """
+    Nonlinear softening function. Is linear at low values.
+    """
+    if lbc < 100.0:
+        return 0.01*lbc
+    return 1.0 + np.log2(lbc/100.0)
+
+#    if lbc < 100.0:
+#        return 0.01*lbc**1.0
+#    elif lbc < 1000.0:
+#        return 0.1*lbc**0.5
+#    elif lbc < 10000.0:
+#        return lbc**0.25
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    x = 10.0**np.linspace(-3.0, 6.0, 100001)
+    y = np.array([soften(_x) for _x in x])
+    plt.semilogx(x, x**0.25, label="Fourth Root")
+    plt.semilogx(x, y, label="This")
+    plt.legend()
+    plt.show()
+
